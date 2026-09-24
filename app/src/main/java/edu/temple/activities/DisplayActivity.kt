b1f90f27e3ebfcfa1f3526d9e7d849348d7bc725
Seth Hoong <seth.hoong@temple.edu>
2026-09-24 14:45:30 -0400

    Open second activity when pressing text view

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -11,9 +11,7 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            textSize = intent.getIntExtra(TEXT_SIZE_KEY, 0).toFloat();
         }
-
-
     }
 }
